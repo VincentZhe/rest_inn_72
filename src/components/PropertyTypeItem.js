c5935d088c068f1126cd32c09092d947fddd2485
Your Name <you@example.com>
2022-03-13 14:40:11 -0400

    updates

--- src/components/PropertyTypeItem.js
+++ src/components/PropertyTypeItem.js
@@ -5,14 +5,15 @@ import { Link } from "react-router-dom";
 const PropertyTypeItem = (props) => {
   return (
     <div className="propertyTypeItem-container">
-      <a href="propertyDescription.html">
+      <Link to={`/propertytype/${props.type}`}>
         <img className="propertyTypeItem-image" src={props.image} />
+
         <div className="propertyTypeItem-info">
           <div className="propertyItem-type">
             <Link to={`/propertytype/${props.type}`}>{props.type}</Link>
           </div>
         </div>
-      </a>
+      </Link>
     </div>
   );
 };
